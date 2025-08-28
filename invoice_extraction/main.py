46386b7c4bd40c75545b6c98892eb089fe91ccd4
SPF-SEPLAG <superintspf@gmail.com>
2025-08-28 16:19:59 -0300

    Split data into chunks for training

--- invoice_extraction/main.py
+++ invoice_extraction/main.py
@@ -13,36 +13,52 @@ tokenizer = Tokenizer()
 #ocr_runner.pipeline()
 #labeler.process_json_files()
 all_encodings = tokenizer.tokenize_labeled()
-# print(all_encodings)
+print(all_encodings)
 
-dataset = TensorDataset(
-    all_encodings["input_ids"],
-    all_encodings["attention_mask"],
-    all_encodings["bbox"],
-    all_encodings["labels"]
-)
+# dataset = TensorDataset(
+#     all_encodings["input_ids"],
+#     all_encodings["attention_mask"],
+#     all_encodings["bbox"],
+#     all_encodings["labels"]
+# )
 
-train_loader = DataLoader(dataset, batch_size=2, shuffle=True)
-#print(train_loader)
-
-for k, batch in enumerate(train_loader):
-    print(f"step: {k}, batch: {batch}")
-    input_ids, attention_mask, bbox, labels = [b for b in batch]
-    print(input_ids)
-    print(attention_mask)
-    print(bbox)
-    print(labels)
-    break
-
-# num_labels = 6  # change according to your dataset
+# train_loader = DataLoader(dataset, batch_size=2, shuffle=True)
+# #print(train_loader)
 
 # model = LayoutLMv3ForTokenClassification.from_pretrained(
 #     "microsoft/layoutlmv3-base",
-#     num_labels=num_labels
+#     num_labels=6
 # )
+# print(model.config.max_position_embeddings)  # should print 512
 
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # model.to(device)
+
+# optimizer = AdamW(model.parameters(), lr=5e-5)
+
+# for k, batch in enumerate(train_loader):
+#     #print(f"step: {k}, batch: {batch}")
+#     input_ids, attention_mask, bbox, labels = [b.to(device) for b in batch]
+
+#     # Forward pass
+#     outputs = model(
+#         input_ids=input_ids,
+#         attention_mask=attention_mask,
+#         bbox=bbox,
+#         labels=labels
+#     )
+    
+#     loss = outputs.loss
+#     logits = outputs.logits  # shape: [batch_size, seq_len, num_labels]
+    
+#     # Backward pass
+#     optimizer.zero_grad()
+#     loss.backward()
+#     optimizer.step()
+    
+#     if k % 2 == 0:  # print every 2 steps
+#         print(f"  Step {k}, Loss: {loss.item():.4f}")
+
 
 # optimizer = AdamW(model.parameters(), lr=5e-5)
 
